05116736499e651f81a2bdd4ee6298ca4fc53218
elaineyeh <elaine81206@gmail.com>
2022-06-19 16:44:24 +0800

    refactor: chat consumer async function

--- channel_proj/chat/consumers.py
+++ channel_proj/chat/consumers.py
@@ -1,44 +1,44 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 
 import json
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.name = self.scope['url_route']['kwargs']['name']
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             'lobby',
             self.channel_name
         )
 
-        self.accept()
-        self.group_send(f'{self.name} join the room.')
+        await self.accept()
+        await self.group_send(f'{self.name} join the room.')
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             'lobby',
             self.channel_name
         )
 
-        self.group_send(f'{self.name} left the room.')
+        await self.group_send(f'{self.name} left the room.')
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = self.name + ': ' + text_data_json['message']
 
-        self.group_send(message)
+        await self.group_send(message)
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
 
-    def group_send(self, msg, group='lobby', type_='chat_message'):
-        async_to_sync(self.channel_layer.group_send)(
+    async def group_send(self, msg, group='lobby', type_='chat_message'):
+        await self.channel_layer.group_send(
             group,
             {
                 'type': type_,
